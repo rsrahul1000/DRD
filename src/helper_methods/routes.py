dcd47dc2af42ad83556486936018ae69fe42ef15
rsrahul1000 <rsrahul1000@gmail.com>
2020-05-03 15:10:43 +0530

    Revert "fix: no need to manually create folders for image saving now"
    This reverts commit 9d62e36f

--- src/helper_methods/routes.py
+++ src/helper_methods/routes.py
@@ -324,7 +324,7 @@ def reset_token(token):
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user.password = hashed_password
         db.session.commit()
-        #session['phone'] = form.phone.data
+        session['phone'] = form.phone.data
         flash(f'Your password has been updated! You are now able to log in', 'success')
         return redirect(url_for('login'))
     return render_template('reset_token.html', title="Reset Password", form=form)
